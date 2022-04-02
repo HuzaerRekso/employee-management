c20deae1a1a23da8aee301655562e73c74e226a9
m.rekso <m.rekso@asyst.co.id>
2022-04-02 10:06:57 +0700

    Initial commit

---
+++ src/test/java/com/huzaer/employeemanagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huzaer.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
